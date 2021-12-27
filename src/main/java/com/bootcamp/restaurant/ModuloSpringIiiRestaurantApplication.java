e5fe4ed43b534739b8823676a0427ef3ce5008d3
Samuel <samuel.stalschus@mercadolivre.com>
2021-12-27 11:25:42 -0300

    Feat: Add - Initial commit

---
+++ src/main/java/com/bootcamp/restaurant/ModuloSpringIiiRestaurantApplication.java
@@ -0,0 +1,13 @@
+package com.bootcamp.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuloSpringIiiRestaurantApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModuloSpringIiiRestaurantApplication.class, args);
+    }
+
+}
